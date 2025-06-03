fcde78ab0dd1bd36b2a657292b66836c270decd3
BMH2000619 <baselmohammedb.m@gmail.com>
2025-06-03 14:59:45 +0300

    added get brand by id

--- controllers/BrandController.js
+++ controllers/BrandController.js
@@ -9,3 +9,13 @@ const GetBrands = async (req, res) => {
     res.status(500).send('Error getting brands')
   }
 }
+
+const GetBrandById = async (req, res) => {
+  try {
+    const brand = await Brand.findById(req.params.brand_id)
+    res.status(200).send(brand)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Error getting brand by ID')
+  }
+}
