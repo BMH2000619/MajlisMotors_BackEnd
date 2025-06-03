51f065489d17b444832639531a47f19906b775c4
BMH2000619 <baselmohammedb.m@gmail.com>
2025-06-03 14:53:17 +0300

    getCarby id

--- controllers/CarController.js
+++ controllers/CarController.js
@@ -9,3 +9,13 @@ const GetCars = async (req, res) => {
     res.status(500).send('Error getting cars')
   }
 }
+
+const GetCarById = async (req, res) => {
+  try {
+    const car = await Car.findById(req.params.car_id).populate('brand_id')
+    res.status(200).send(car)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Error getting car by ID')
+  }
+}
