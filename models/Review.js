ade94ad6eeb1c4668c750936c42d3c96ec625912
BMH2000619 <baselmohammedb.m@gmail.com>
2025-06-03 09:22:47 +0300

    backend added reviewSchema

---
+++ models/Review.js
@@ -0,0 +1,24 @@
+const { Schema } = require('mongoose')
+
+const reviewSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true
+    },
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    car_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Car'
+    },
+    rating: {
+      type: Number
+    }
+  },
+  { timestamps: true }
+)
+
+module.exports = reviewSchema
