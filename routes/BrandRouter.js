2ff1261cdccac438a9f647d5f0c1f455f61edb4b
BMH2000619 <baselmohammedb.m@gmail.com>
2025-06-03 20:58:30 +0300

    added post and put router to brand

--- routes/BrandRouter.js
+++ routes/BrandRouter.js
@@ -7,3 +7,19 @@ router.get('/', controller.GetBrands)
 
 // GET a single brand by ID
 router.get('/:brand_id', controller.GetBrandById)
+
+// CREATE a new brand (protected route)
+router.post(
+  '/',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.CreateBrand
+)
+
+// UPDATE a brand by ID (protected route)
+router.put(
+  '/:brand_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.UpdateBrand
+)
