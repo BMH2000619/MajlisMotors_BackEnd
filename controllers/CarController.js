e33d73eada5fabdb5c4355870b915e04a3a206f6
BMH2000619 <baselmohammedb.m@gmail.com>
2025-06-03 20:34:33 +0300

    updated Carcontroller

--- controllers/CarController.js
+++ controllers/CarController.js
@@ -1,35 +1,46 @@
 const { Car } = require('../models')
 
+// GET all cars (with optional brand population)
 const GetCars = async (req, res) => {
   try {
-    const cars = await Car.find().populate('brand_id')
+    const cars = await Car.find().populate('brand_id', 'name logo')
     res.status(200).send(cars)
   } catch (error) {
     console.error(error)
-    res.status(500).send('Error getting cars')
+    res.status(500).send({ error: 'Failed to fetch cars' })
   }
 }
 
+// GET car by ID (with brand and favorites info)
 const GetCarById = async (req, res) => {
   try {
-    const car = await Car.findById(req.params.car_id).populate('brand_id')
+    const car = await Car.findById(req.params.car_id)
+      .populate('brand_id', 'name logo')
+      .populate('favorites', 'name email image')
+
+    if (!car) {
+      return res.status(404).send({ error: 'Car not found' })
+    }
+
     res.status(200).send(car)
   } catch (error) {
     console.error(error)
-    res.status(500).send('Error getting car by ID')
+    res.status(500).send({ error: 'Failed to fetch car' })
   }
 }
 
+// CREATE a new car
 const CreateCar = async (req, res) => {
   try {
-    const car = await Car.create({ ...req.body })
+    const car = await Car.create(req.body)
     res.status(201).send(car)
   } catch (error) {
     console.error(error)
-    res.status(500).send('Error creating car')
+    res.status(500).send({ error: 'Failed to create car' })
   }
 }
 
+// UPDATE an existing car
 const UpdateCar = async (req, res) => {
   try {
     const car = await Car.findByIdAndUpdate(req.params.car_id, req.body, {
@@ -38,17 +49,22 @@ const UpdateCar = async (req, res) => {
     res.status(200).send(car)
   } catch (error) {
     console.error(error)
-    res.status(500).send('Error updating car')
+    res.status(500).send({ error: 'Failed to update car' })
   }
 }
 
+// DELETE a car
 const DeleteCar = async (req, res) => {
   try {
     await Car.deleteOne({ _id: req.params.car_id })
-    res.send({ msg: 'Car deleted', id: req.params.car_id })
+    res.send({
+      msg: 'Car deleted',
+      payload: req.params.car_id,
+      status: 'Ok'
+    })
   } catch (error) {
     console.error(error)
-    res.status(500).send('Error deleting car')
+    res.status(500).send({ error: 'Failed to delete car' })
   }
 }
 
